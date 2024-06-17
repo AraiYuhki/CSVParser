4c67c645ed98a53c4733f0589ac912646fb85943
xeon_lagunas <xeon.lagunas@gmail.com>
2024-06-17 14:21:14 +0900

    自作クラスにも対応できるように修正

--- Packages/SimpleCSVParser/Runtime/CsvParser.cs
+++ Packages/SimpleCSVParser/Runtime/CsvParser.cs
@@ -16,6 +16,29 @@ namespace Xeon.IO
 
         private static readonly Regex StringRegex = new Regex("(\".+\")");
         private static readonly Regex VectorRegex = new Regex(@"(\([\d\.\,\-]+\))\,");
+        private static readonly Regex ListRegex = new Regex(@"\[(.+)\]");
+        private static readonly Regex ObjectRegex = new Regex(@"{(.+)}");
+        private static Dictionary<Regex, string> escapeRegexs = new()
+        {
+            { VectorRegex , "vector" },
+            { ListRegex , "list" },
+            { ObjectRegex, "object" },
+        };
+
+        public static void AddEscapeRegex(string name, Regex regex)
+        {
+            escapeRegexs.Remove(regex);
+            escapeRegexs.Add(regex, name);
+        }
+
+        public static void RemoveEscapeRegex(Regex regex) => escapeRegexs.Remove(regex);
+
+        public static void ResetEscapeRegex()
+        {
+            escapeRegexs.Clear(); ;
+            AddEscapeRegex("vector", VectorRegex);
+            AddEscapeRegex("list", ListRegex);
+        }
 
         private string csv = null;
         private string separator = null;
@@ -25,8 +48,9 @@ namespace Xeon.IO
         {
             this.csv = csv.Replace("\r\n", "\n").Replace("\r", "\n");
             this.separator = separator;
-            Escape(StringRegex, "<escaped string>{0}</escaped string>");
-            Escape(VectorRegex, "<escaped vector>{0}</escaped string>");
+            EscapeString();
+            foreach (var (regex, name) in escapeRegexs)
+                Escape(regex, $"<escaped {name}>{{0}}</escaped {name}>");
         }
 
         public string GetResult() => csv;
@@ -46,7 +70,35 @@ namespace Xeon.IO
                 csv = csv.Replace(before, after);
                 escapedData.Add(after, before);
             }
-            Debug.Log(csv);
+        }
+
+        private void EscapeString()
+        {
+            var replaceTexts = new List<string>();
+            var startIndex = -1;
+            for (var index = 0; index < csv.Length; index++)
+            {
+                if (startIndex < 0)
+                {
+                    if (csv[index] != '"') continue;
+                    startIndex = index;
+                    continue;
+                }
+                if (csv[index] != '"') continue;
+                var endIndex = index + 1;
+                var target = csv[startIndex..endIndex];
+                var escapeIndex = replaceTexts.IndexOf(target);
+                if (escapeIndex < 0)
+                {
+                    escapeIndex = escapedData.Count;
+                    replaceTexts.Add(target);
+                }
+                var replaceText = $"<escaped string>{escapeIndex}</escaped string>";
+                csv = csv.Replace(target, replaceText);
+                if (!escapedData.ContainsKey(replaceText))
+                    escapedData.Add(replaceText, target);
+                startIndex = -1;
+            }
         }
 
         public static List<T> Parse<T>(string csv, string separator = "\t") where T : CsvData, new()
@@ -82,9 +134,19 @@ namespace Xeon.IO
                     isFirst = false;
                     continue;
                 }
-                var parsed = headers.Select((key, index) => (key, columns[index])).ToDictionary(pair => pair.key, pair => pair.Item2);
-                var instance = CreateInstance<T>(attributes, members, type, parsed);
-                result.Add(instance);
+                try
+                {
+                    var parsed = headers.Select((key, index) => (key, columns[index])).ToDictionary(pair => pair.key, pair => pair.Item2);
+                    var instance = CreateInstance<T>(attributes, members, type, parsed);
+                    result.Add(instance);
+                }catch(Exception e)
+                {
+                    Debug.LogException(e);
+                    Debug.LogError(string.Join(",", headers));
+                    Debug.LogError(string.Join(",", columns));
+                    foreach (var (key, data) in escapedData)
+                        Debug.LogError($"{key}:{data}");
+                }
             }
             return result;
         }
@@ -113,13 +175,15 @@ namespace Xeon.IO
             foreach (var (key, value) in row)
             {
                 var text = value;
-                if (escapedData.ContainsKey(text)) text = escapedData[text];
                 if (!attributes.ContainsKey(key) || !members.ContainsKey(key)) continue;
                 var member = members[key];
                 try
                 {
                     if (member.MemberType == MemberTypes.Property)
+                    {
+                        text = RestoreEscape(text);
                         type.GetProperty(member.Name).SetValue(instance, text);
+                    }
                     else if (member.MemberType == MemberTypes.Field)
                         SetValue(type, member.Name, instance, text);
                     else
@@ -134,17 +198,63 @@ namespace Xeon.IO
             return instance;
         }
 
-        private static void SetValue<T>(Type type, string memberName, T instance, string value)
+        private string RestoreEscape(string text)
+        {
+            while(true)
+            {
+                var isReplaced = false;
+                foreach (var (escaped, origin) in escapedData)
+                {
+                    if (text.Contains(escaped))
+                    {
+                        text = text.Replace(escaped, origin);
+                        isReplaced = true;
+                    }
+                }
+                if (!isReplaced)
+                    return text;
+            }
+        }
+
+        private string Restore(string text, string escapeTarget)
+        {
+            while(true)
+            {
+                var isReplaced = false;
+                foreach (var (escaped, origin) in escapedData)
+                {
+                    if (!escaped.Contains(escapeTarget)) continue;
+                    if (text.Contains(escaped))
+                    {
+                        text = text.Replace(escaped, origin);
+                        isReplaced = true;
+                    }
+                }
+                if (!isReplaced)
+                    return text;
+            }
+        }
+
+        private void SetValue<T>(Type type, string memberName, T instance, string value)
         {
             var fieldInfo = type.GetField(memberName, ProperyFlags);
-            if (fieldInfo.FieldType == typeof(int) && int.TryParse(value, out var intValue))
+            value = Restore(value, "string");
+            value = Restore(value, "object");
+            value = Restore(value, "list");
+            if (fieldInfo.FieldType.GetInterface(nameof(ICsvSupport)) != null)
+            {
+                var data = (ICsvSupport)Activator.CreateInstance(fieldInfo.FieldType);
+                data.FromCsv(Restore(value, "string"));
+                fieldInfo.SetValue(instance, data);
+            }
+            else if (fieldInfo.FieldType == typeof(int) && int.TryParse(value, out var intValue))
                 fieldInfo.SetValue(instance, intValue);
             else if (fieldInfo.FieldType == typeof(float) && float.TryParse(value, out var floatValue))
                 fieldInfo.SetValue(instance, floatValue);
             else if (fieldInfo.FieldType == typeof(bool) && bool.TryParse(value, out var boolValue))
                 fieldInfo.SetValue(instance, boolValue);
             else if (fieldInfo.FieldType == typeof(string))
-                fieldInfo.SetValue(instance, value.Trim('"'));
+                fieldInfo.SetValue(instance, value.FromCsv());
             else if (fieldInfo.FieldType.IsGenericType && fieldInfo.FieldType.GetGenericTypeDefinition() == typeof(List<>))
                 SetArrayValue(fieldInfo, instance, value);
             else if (fieldInfo.FieldType.IsEnum)
@@ -163,12 +273,15 @@ namespace Xeon.IO
                 throw new Exception($"{fieldInfo.FieldType} is not supported");
         }
 
-        private static void SetArrayValue<T>(FieldInfo fieldInfo, T instance, string value)
+        private void SetArrayValue<T>(FieldInfo fieldInfo, T instance, string value)
         {
+            value = value.Trim('[', ']');
             if (fieldInfo.FieldType.GenericTypeArguments.Length > 1)
                 throw new InvalidCastException("Multi generic arguments type is not support");
             var type = fieldInfo.FieldType.GenericTypeArguments[0];
-            var splited = value.Split(",").Where(splited => !string.IsNullOrEmpty(splited));
+            var splited = value.Split(",").Where(splited => !string.IsNullOrEmpty(splited)).ToList();
+            for (var index = 0; index < splited.Count; index++)
+                splited[index] = Restore(splited[index], "string");
             if (type == typeof(int))
                 fieldInfo.SetValue(instance, splited.Select(data => int.Parse(data)).ToList());
             else if (type == typeof(float))
@@ -176,7 +289,7 @@ namespace Xeon.IO
             else if (type == typeof(bool))
                 fieldInfo.SetValue(instance, splited.Select(data => bool.Parse(data)).ToList());
             else if (type == typeof(string))
-                fieldInfo.SetValue(instance, splited.Select(data => data.Trim('"')).ToList());
+                fieldInfo.SetValue(instance, splited.Select(data => data.FromCsv()).ToList());
             else if (type.IsEnum)
                 fieldInfo.SetValue(instance, splited.Select(data => Enum.Parse(type, data)).ToList());
             else
@@ -201,37 +314,11 @@ namespace Xeon.IO
                         value = type.GetField(member.Name, ProperyFlags).GetValue(row);
                     else
                         continue;
-                    values.Add(ToString(value));
+                    values.Add(CsvSupport.ToString(value));
                 }
                 builder.AppendLine(string.Join(separator, values));
             }
             return builder.ToString();
-        }
-
-        private static string ToString(object value, string separator = ",")
-        {
-            if (value is string)
-                return $"\"{value}\"";
-            if (value is Vector2 vector2)
-                return vector2.ToCsv(separator);
-            if (value is Vector3 vector3)
-                return vector3.ToCsv(separator);
-            if (value is Vector2Int vector2Int)
-                return vector2Int.ToCsv(separator);
-            if (value is Vector3Int vector3Int)
-                return vector3Int.ToCsv(separator);
-            if (value is Vector4 vector4)
-                return vector4.ToCsv(separator);
-            if (value is Quaternion quaternion)
-                return quaternion.ToCsv(separator);
-            if (value is not string && value is IEnumerable enumerable)
-            {
-                var array = new List<object>();
-                foreach (var data in enumerable)
-                    array.Add(data);
-                return $"[{string.Join(separator, array.ToArray())}]";
-            }
-            return value.ToString();
         }
     }
 }
--- Packages/SimpleCSVParser/Test/CsvParseTest.cs
+++ Packages/SimpleCSVParser/Test/CsvParseTest.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 using Xeon.IO;
@@ -15,6 +16,41 @@ public class CsvParseTest
         One,
         Two,
     }
+
+    private class TestValue : ICsvSupport
+    {
+        public static Regex EscapeRegex => new Regex(@"({{.+}})");
+        public int id;
+        public string name;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not TestValue other)
+                return false;
+            if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(other.name))
+                return id == other.id;
+            return id == other.id && name == other.name;
+        }
+
+        public string ToCsv(string separator = ",")
+        {
+            var name = $"\"{this.name}\"";
+            return $"{{{string.Join(separator, id, name)}}}";
+        }
+        public void FromCsv(string csv)
+        {
+            var tmp = csv.Trim('{', '}');
+            var splited = tmp.Split(',');
+            id = int.Parse(splited[0]);
+            name = string.Join(",", splited.Skip(1).Select(text => text.Trim('"')));
+        }
+
+        public override string ToString()
+        {
+            return $"{id}, {name}";
+        }
+    }
+
     [Serializable]
     private class TestData : CsvData
     {
@@ -45,6 +81,42 @@ public class CsvParseTest
         public TestData(int privateValue)
             => this.privateValue = privateValue;
     }
+
+    [Serializable]
+    private class TestData2 : CsvData
+    {
+        [CsvColumn("list_value")]
+        public List<string> listValue = new();
+        [CsvColumn("class_value")]
+        public TestValue classValue;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not TestData2 other)
+                return false;
+            if (listValue == null || other.listValue == null)
+                return false;
+            if (listValue.Count != other.listValue.Count)
+                return false;
+            foreach (var (item, index) in listValue.Select((item, index) => (item, index)))
+            {
+                if (item != other.listValue[index])
+                    return false;
+            }
+            if (classValue == null && other.classValue == null) return true;
+            if (classValue != null && other.classValue != null)
+                return classValue.Equals(other.classValue);
+            return false;
+        }
+
+        public override string ToString()
+        {
+            var list = string.Join(",", listValue);
+            var classText = $"{classValue.id}::{classValue.name}";
+            return $"listValue:{list}, classValue:{classText}";
+        }
+    }
+
     [Test]
     public void ToCsvTest()
     {
@@ -61,7 +133,7 @@ public class CsvParseTest
             vector4Value = new Vector4(1.1f, 2.2f, 3.3f, 4),
             enumValue = TestEnum.Two
         };
-        var csv = CsvParser.ToCSV(new List<TestData>(){ testData }, ",");
+        var csv = CsvParser.ToCSV(new List<TestData>() { testData }, ",");
         var expect = @"int_value,float_value,string_value,bool_value,vector2_value,vector2int_value,vector3_value,vector3int_value,vector4_value,enum_value,private_int_value
 10,123.45,""abcdefg"",False,(10,10.5),(0,-1),(1.1,2.2,3.3),(1,1,1),(1.1,2.2,3.3,4),Two,12
 ";
@@ -97,5 +169,42 @@ public class CsvParseTest
         Assert.That(data.vector3IntValue == Vector3Int.one);
         Assert.That(data.vector4Value == new Vector4(1.1f, 2.2f, 3.3f, 4));
         Assert.That(data.enumValue == TestEnum.Two);
+    }
+
+    [Test]
+    public void ListAndClassParseTest()
+    {
+        var obj = new List<TestData2>(){
+            new TestData2() {
+                listValue = new() { "abd", "xeon", "tesst" },
+                classValue = new TestValue() { id = -1, name = "TestValue" }
+            },
+            new TestData2()
+            {
+                listValue = new(){"array1,array2", "test"},
+                classValue = new TestValue(){id = 10, name = "test,test2"}
+            },
+            new TestData2()
+            {
+                listValue = new(),
+                classValue = new TestValue()
+            }
+        };
+
+        var csvExpect = @"list_value,class_value
+[""abd"",""xeon"",""tesst""],{-1,""TestValue""}
+[""array1,array2"",""test""],{10,""test,test2""}
+[],{0,""""}
+";
+        var csv = CsvParser.ToCSV(obj, ",");
+        Assert.That(csv == csvExpect);
+        var actual = CsvParser.Parse<TestData2>(csv, ",");
+        Assert.That(obj.Count == actual.Count);
+        foreach (var (data, index) in actual.Select((data, index) => (data, index)))
+        {
+            if (data == null || obj[index] == null)
+                continue;
+            Assert.That(data.Equals(obj[index]));
+        }
     }
 }
